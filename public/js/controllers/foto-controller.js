2bb2f7d67e8dba746373624c35d57c0620d691c2
Alexandre Santos <alexandre.santos@projecti.com.br>
2018-12-10 01:12:41 -0200

    capitulo  7 part 2

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,17 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
 
     $scope.foto = {};
 
     $scope.submeter = function(){
-        console.log($scope.foto)
+        $http.post('v1/fotos', $scope.foto)
+        .success(function(){
+            $scope.foto ={};
+            console.log('Foto cadastrada com sucesso');
+        })
+        .error(function(erro){
+            console.log(erro)
+        })
+
     };
 
 });
